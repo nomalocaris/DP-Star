e0d9520d65faa4adc97c438bc1a5895d4540bd61
许继元 <490601115@qq.com>
2021-04-16 23:48:05 +0800

    feat: signum

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,3 +6,15 @@ from __future__ import (absolute_import, unicode_literals)
 from ._progressbar import ProgressBar
 
 
+def signum(x):
+    """cal signum
+
+    :param x:
+    :return:
+    """
+    if x > 0:
+        return 1.0
+    if x < 0:
+        return -1.0
+    if x == 0:
+        return 0
