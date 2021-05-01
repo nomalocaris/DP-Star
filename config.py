cf2901934a1377283aba83385dab846f6266b616
foolishkylin <foolishkylin@gmail.com>
2021-05-01 16:43:12 +0800

    update the config file for mdl1100

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from __future__ import (absolute_import, unicode_literals)
 import os
 
 # privacy budget
-epsilon = 0.1
+epsilon = 2.0
 
 # budget allocation
 epsilon_alloc = {
@@ -19,7 +19,7 @@ epsilon_alloc = {
 }
 
 # trajectory geo range
-gps_range = {'lon': (0, 359.9955), 'lat': (0, 479.99499)}
+gps_range = {'lat': (0, 1320), 'lon': (0, 1760)}
 
 # the number of the top grid
 n_top_grid = 7
@@ -28,16 +28,21 @@ n_top_grid = 7
 beta_factor = 80
 
 # files path
-idir_mdl_traj = 'data/Geolife Trajectories 1.3/representative_point'  # the mdl traj file
-opath_top_grid = 'data/Geolife Trajectories 1.3/middleware/top_grid.txt'  # the ada grid construction
-opath_grid_traj = 'data/Geolife Trajectories 1.3/middleware/grid_traj.txt'  # the grid-lized traj
-omega_path = 'data/Geolife Trajectories 1.3/middleware/grid_block_gps_range.txt'  # the top grid range
-r_path = 'data/Geolife Trajectories 1.3/middleware/trip_distribution.txt'
-x_path = 'data/Geolife Trajectories 1.3/middleware/midpoint_movement.txt'
-l_path = 'data/Geolife Trajectories 1.3/middleware/length_traj.txt'
-sd_path = 'data/Geolife Trajectories 1.3/middleware/sd.txt'  # grid-lized sd traj
-sd_final_path = 'data/Geolife Trajectories 1.3/sd/sd_final_ep' + str(epsilon)  # ture sd traj dir
+idir_mdl_traj = 'data/Geolife Trajectories 1.3/MDL1100'  # the mdl traj file
+# the ada grid construction
+opath_top_grid = 'data/Geolife Trajectories 1.3/middleware/top_grid_MDL1100_ep' + str(epsilon) + '.txt'
+# the grid-lized traj
+opath_grid_traj = 'data/Geolife Trajectories 1.3/middleware/grid_traj_MDL1100_ep' + str(epsilon) + '.txt'
+# the top grid range
+omega_path = 'data/Geolife Trajectories 1.3/middleware/grid_block_gps_range_MDL1100_ep' + str(epsilon) + '.txt'
+r_path = 'data/Geolife Trajectories 1.3/middleware/trip_distribution_MDL1100_ep' + str(epsilon) + '.txt'
+x_path = 'data/Geolife Trajectories 1.3/middleware/midpoint_movement_MDL1100_ep' + str(epsilon) + '.txt'
+l_path = 'data/Geolife Trajectories 1.3/middleware/length_traj_MDL1100_ep' + str(epsilon) + '.txt'
+sd_path = 'data/Geolife Trajectories 1.3/middleware/sd_MDL1100_ep' + str(epsilon) + '.txt'  # grid-lized sd traj
+sd_final_path = 'data/Geolife Trajectories 1.3/sd/sd_final_MDL1100_ep' + str(epsilon)  # ture sd traj dir
 
 # create sd final path
 if not os.path.exists(sd_final_path):
     os.makedirs(sd_final_path)
+
+
