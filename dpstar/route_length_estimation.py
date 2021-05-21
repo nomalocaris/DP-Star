71a04a597d2984ffc525064d5523fa06b57b8a80
许继元 <490601115@qq.com>
2021-05-21 17:40:12 +0800

    chore: modify code

--- dpstar/route_length_estimation.py
+++ dpstar/route_length_estimation.py
@@ -11,14 +11,12 @@
 
 import random
 
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 
 from utils import ProgressBar
 
 
-def exp_mechanism(score, m, epsilon, sensitivity):
+def exp_mechanism(score, m: int, _epsilon: float, sensitivity: float) -> int:
     """
 
     指数机制，选出一个符合定义的下标
@@ -26,10 +24,11 @@ def exp_mechanism(score, m, epsilon, sensitivity):
     Args:
         score      :
         m          :
-        epsilon    :
+        _epsilon   :
         sensitivity:
 
     Returns:
+        j:
 
     """
     exponents_list = [0 for _ in range(m)]
@@ -37,7 +36,7 @@ def exp_mechanism(score, m, epsilon, sensitivity):
     sum_exp = 0
 
     for i in range(m):
-        expo = 0.5 * (score[i]) * epsilon / sensitivity
+        expo = 0.5 * (score[i]) * _epsilon / sensitivity
         exponents_list[i] = np.exp(expo)
 
         summary += exponents_list[i]
@@ -55,21 +54,29 @@ def exp_mechanism(score, m, epsilon, sensitivity):
     return j
 
 
-def route_length_estimate(trajectory, A, lo, hi, epsilon, sensitivity):
+def route_length_estimate(trajs: list, n_grid: int, lo, hi: float, _epsilon: float,
+                          sensitivity: float) -> list:
     """
 
     轨迹长度估计
 
     Args:
+        trajs      :
+        n_grid     :
+        lo         :
+        hi         :
+        _epsilon   :
+        sensitivity:
 
     Returns:
+        L:
 
     """
-    C = A * A
+    C = n_grid * n_grid
     L_matrix = [[] for _ in range(C)]  # L矩阵
-    L_array = []
+    L = []
 
-    for t in trajectory:
+    for t in trajs:
         lenT = len(t)
         if lenT > hi:
             continue
@@ -78,7 +85,7 @@ def route_length_estimate(trajectory, A, lo, hi, epsilon, sensitivity):
 
         row = t[0]
         col = t[-1]
-        l_index = row * A + col  # 转一维坐标
+        l_index = row * n_grid + col  # 转一维坐标
         L_matrix[l_index].append(lenT)
 
     p = ProgressBar(C, '计算轨迹中值长度矩阵')
@@ -88,53 +95,55 @@ def route_length_estimate(trajectory, A, lo, hi, epsilon, sensitivity):
         K = L_matrix[i].copy()  # 取一种头尾轨迹的所有轨迹长
         K.sort()  # 顺序排序
         if len(K) < 1:
-            L_array.append(0)
+            L.append(0)
             continue
         m_index = len(K) / 2  # 中值下标
         for j in range(len(K)):
             score_arr.append(-abs(j - m_index))  # 得分函数
-        r_index = exp_mechanism(score_arr, len(K), epsilon, sensitivity)
+        r_index = exp_mechanism(score_arr, len(K), _epsilon, sensitivity)
         # print(K, '--->', K[r_index])
-        L_array.append(K[r_index])
+        L.append(K[r_index])
 
-    return L_array
+    return L
 
 
-def route_length_estimate_main(A, epsilon, src_file, out_file):
+def route_length_estimate_main(n_grid: int, _epsilon: float, grid_trajs_path: str,
+                               routes_length_path: str) -> int:
     """
 
     主函数
 
     Args:
+        n_grid            : 网格数
+        _epsilon          : 隐私预算
+        grid_trajs_path   : 网格轨迹文件路径
+        routes_length_path: 轨迹长度估计文件路径
 
     Returns:
+        maxT: 最大轨迹长度
 
     """
-    with open(src_file, 'r') as grid_trajectories_file:
+    with open(grid_trajs_path, 'r') as grid_trajs_file:
         # 网格轨迹数据(list)
-        T = [eval(grid_trajectory) for grid_trajectory in grid_trajectories_file.readlines()]
+        T = [eval(grid_traj) for grid_traj in grid_trajs_file.readlines()]
         maxT = max([len(i) for i in T])
-        with open(out_file, 'w') as route_length_file:
-            l_array = route_length_estimate(T, A, 0, 1.25 * maxT, epsilon, 1)
+        with open(routes_length_path, 'w') as routes_length_file:
+            l_array = route_length_estimate(T, n_grid, 0, 1.25 * maxT, _epsilon, 1.0)
             len_modify_func = lambda x: x if x >= 2 else 2
             l_array = [len_modify_func(x) for x in l_array]
-            l_mat = np.array(l_array).reshape((A, A))
+            l_mat = np.array(l_array).reshape((n_grid, n_grid))
             for arr in l_mat:
                 for i in range(len(arr)):
                     if i < len(arr) - 1:
-                        route_length_file.write(str(arr[i])+' ')
+                        routes_length_file.write(str(arr[i]) + ' ')
                     else:
-                        route_length_file.write(str(arr[i]) + '\n')
-    sns.heatmap(data=l_mat, square=True)
-    plt.title('route length estimation (epsilon=%s)' % str(used_pair[0]))
-    plt.show()
+                        routes_length_file.write(str(arr[i]) + '\n')
 
     return maxT
 
 
 if __name__ == '__main__':
-    ep_grid_pairs = ((0.1, 67), (0.5, 120), (1.0, 193), (2.0, 364))
-    used_pair = ep_grid_pairs[0]
-    route_length_estimate_main(used_pair[1], used_pair[0] * 2 / 9,
-                               f'../data/Geolife Trajectories 1.3/middleware/grid_traj_MDL1100_ep{used_pair[0]}.txt',
-                               f'../data/Geolife Trajectories 1.3/middleware/length_traj_MDL1100_ep{used_pair[0]}.txt')
+    epsilon = 0.1
+    route_length_estimate_main(64, epsilon * 2 / 9,
+                               f'../data/Geolife Trajectories 1.3/Middleware/grid_trajs_epsilon_{epsilon}.txt',
+                               f'../data/Geolife Trajectories 1.3/Middleware/routes_length_epsilon_{epsilon}.txt')
