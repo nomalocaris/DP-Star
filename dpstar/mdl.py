1ff5adbd52964b69af39f44e60d102b9239be432
许继元 <490601115@qq.com>
2021-05-23 14:54:22 +0800

    refactor: by Giyn

--- dpstar/mdl.py
+++ dpstar/mdl.py
@@ -1,31 +1,54 @@
-#!/usr/bin/env python
-# -*-coding:utf-8-*-
-# Author: nomalocaris <nomalocaris.top>
-""""""
-import numpy as np
+"""
+-------------------------------------
+# -*- coding: utf-8 -*-
+# @Author  : nomalocaris、Giyn、HZT
+# @File    : mdl.py
+# @Software: PyCharm
+-------------------------------------
+"""
+
 import os
-from utils import vlen, ProgressBar, to_vec_add, to_vec_sub, to_vec_times, to_vec_dot
-from joblib import Parallel, delayed
+
+import numpy as np
+
+from utils import vlen
+from utils import ProgressBar
+from utils import to_vec_add
+from utils import to_vec_sub
+from utils import to_vec_times
+from utils import to_vec_dot
 
 
 def lt(t_traj, start_ind, curr_ind):
-    """cal L(T~)
-    :param t_traj:
-    :param start_ind:
-    :param curr_ind:
-    :return:
+    """
+
+    cal L(T~)
+
+    Args:
+        t_traj   :
+        start_ind:
+        curr_ind :
+
+    Returns:
+
+
     """
     vlength = vlen(t_traj[start_ind], t_traj[curr_ind])
+
     return np.log2(vlength) if vlength else np.spacing(1)
 
 
 def cal_perpendicular(si, ei, sj, ej):
     """
-    :param si:
-    :param ei:
-    :param sj:
-    :param ej:
-    :return:
+
+    Args:
+        si:
+        ei:
+        sj:
+        ej:
+
+    Returns:
+
     """
     sisj = to_vec_sub(sj, si)
     siei = to_vec_sub(ei, si)
@@ -43,16 +66,23 @@ def cal_perpendicular(si, ei, sj, ej):
         outD = 0
     else:
         outD = (lp1 ** 2 + lp2 ** 2) / (lp1 + lp2)
+
     return outD
 
 
 def angular(si, ei, sj, ej):
-    """cal angular distance
-    :param si:
-    :param ei:
-    :param sj:
-    :param ej:
-    :return:
+    """
+
+    cal angular distance
+
+    Args:
+        si:
+        ei:
+        sj:
+        ej:
+
+    Returns:
+
     """
     siei = to_vec_sub(ei, si)
     sjej = to_vec_sub(ej, sj)
@@ -67,15 +97,22 @@ def angular(si, ei, sj, ej):
         else:
             sin0 = 0
         outD = np.sqrt(to_vec_dot(sjej, sjej)) * sin0
+
     return outD
 
 
 def lttilde(t_traj, start_ind, curr_ind):
-    """cal L(T|T~)
-    :param t_traj:
-    :param start_ind:
-    :param curr_ind:
-    :return:
+    """
+
+    cal L(T|T~)
+
+    Args:
+        t_traj   :
+        start_ind:
+        curr_ind :
+
+    Returns:
+
     """
     score1 = 0
     score2 = 0
@@ -99,18 +136,25 @@ def lttilde(t_traj, start_ind, curr_ind):
 
 
 def Tmdl(t_traj):
-    """generate func, see: Trajectory Clustering: A Partition-and-Group Framework
-    计算得到的用代表点表示的轨迹
-    :param t_traj:
-    :return:
+    """计算得到的用代表点表示的轨迹
+
+    generate func, see: Trajectory Clustering: A Partition-and-Group Framework
+
+    Args:
+        t_traj:
+
+    Returns:
+
     """
     Tlen = len(t_traj)  # 原始的点数
     if Tlen == 0:
         return []
+
     CP = list()
     CP.append(t_traj[0])    # 存放初始轨迹点
     startIndex = 0      # 原点
     length = 1
+
     while startIndex + length < Tlen:
         currIndex = startIndex + length
         if lttilde(t_traj, startIndex, currIndex) > 0:
@@ -120,19 +164,33 @@ def Tmdl(t_traj):
         else:
             length += 1
     CP.append(t_traj[-1])
+
     return CP
 
 
-def mdl_main(min_latitude, min_longitude, init_path, base_path_list, preserve_path, rate=300, pid=0):
-    """main func
+def mdl_main(min_latitude, min_longitude, init_path, preserve_path, rate):
     """
-    # base_path_list = os.listdir(init_path)
-    # tot_len = len(base_path_list)           # 有14650个
-    # p = ProgressBar(tot_len, 'MDL轨迹简化')
-    cnt = 0
-    for path in base_path_list:
-        # path = base_path_list[i]
-        # p.update(i)
+
+    main func
+
+    Args:
+        min_latitude :
+        min_longitude:
+        init_path    :
+        preserve_path:
+        rate         :
+
+    Returns:
+
+    """
+    if not os.path.exists(preserve_path):
+        os.makedirs(preserve_path)
+    base_path_list = os.listdir(init_path)
+    tot_len = len(base_path_list)           # 有14650个
+    p = ProgressBar(tot_len, 'MDL轨迹简化')
+    for i in range(tot_len):
+        path = base_path_list[i]
+        p.update(i)
         # 打开独个轨迹数据
         with open(init_path + path, 'r') as file_object:
             T = []
@@ -140,21 +198,19 @@ def mdl_main(min_latitude, min_longitude, init_path, base_path_list, preserve_pa
                 jw = line.strip().split(',')
                 w = float(jw[0].strip())
                 j = float(jw[1].strip())
-                T.append(((w - min_latitude)*rate, (j - min_longitude)*rate))       # 对轨迹点做映射
-            t_tilde = Tmdl(T)      # 生成的轨迹
+                T.append(((w - min_latitude)*rate, (j - min_longitude)*rate))  # 对轨迹点做映射
+            t_tilde = Tmdl(T)  # 生成的轨迹
             # 查看生成的轨迹的长度，并检查有无空集
+            print("代表点的长度", len(t_tilde))
             if not len(t_tilde):
                 print(init_path + path)
                 print(T)
             with open(preserve_path + path.strip().split('.')[0] + '.txt', 'w') as f3:
                 for item in t_tilde:
                     f3.writelines(str(item) + '\n')
-            cnt += 1
-            if cnt % 100 == 0:
-                print('process: %d traj num %d' % (pid, cnt))
 
 
-def cheak(path='../data/Geolife Trajectories 1.3/MDL/'):
+def cheak(path='../data/QG Taxi/MDL/'):
     base_path_list = os.listdir(path)
     for i in range(len(base_path_list)):
         path_ = base_path_list[i]
@@ -163,34 +219,20 @@ def cheak(path='../data/Geolife Trajectories 1.3/MDL/'):
                 print("空的：", path_)
 
 
-def check_data(check_path='../data/Geolife Trajectories 1.3/MDL1200/'):
+def check_data():
     length_ = []
-    base_path_list = os.listdir(check_path)
+    base_path_list = os.listdir('../data/QG Taxi/MDL/')
     for path in base_path_list:
-        file_object = open(check_path + path, 'r')
+        file_object = open('../data/QG Taxi/MDL/' + path, 'r')
         length_.append(len(file_object.readlines()))
-    print(length_[:20])
+    print(length_)
     print(np.mean(length_))
     print(np.std(length_, ddof=1))
 
 
 if __name__ == '__main__':
-    # 参数min_latitude, max_latitude, len_latitude纬度长度, min_longitude, max_longitude, len_longitude经度长度,
-    # [39.4, 41.6, 115.7, 117.4] 总的轮廓 t_fullv2 953.054 1761.367  mdl_fullv2 40.952 56.869 1200
-    # [39.7, 40.2, 116.0, 116.8] 最密集的地方 t_densev1 903.496 1658.663  mdl_densev1 38.368 51.333 1200
-    # [39.7, 41.6, 115.7, 117.4] 最密集往左 t_leftv2 943.022 1789.898
-    # [39.4, 40.2, 115.7, 117.4] 最密集往右 t_rightv1 910.620 1623.375
-    # [39.4, 41.6, 116.0, 117.4] 最密集往上 t_upv1 951.595 1718.188
-    # [39.4, 41.6, 115.7, 116.8] 最密集往下 t_downv1 937.325 1704.389
-    base_path = '../data/Geolife Trajectories 1.3/t_leftv2/'
-    new_path = '../data/Geolife Trajectories 1.3/mdl_leftv1/'
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
-
-    n_worker = 5
-    base_path_list = os.listdir(base_path)
-    Parallel(n_jobs=n_worker)(delayed(mdl_main)
-                              (39.7, 116.0, base_path, fs, new_path, 1100, pid)
-                              for fs, pid in zip(np.array_split(base_path_list, n_worker),
-                                                 range(n_worker)))
-    check_data(new_path)
+    mdl_main(22.8, 112.7,
+             '../data/QG Taxi/Trajectories/',
+             '../data/QG Taxi/MDL/', 1200)
+    cheak()
+    check_data()
--- dpstar/trip_distribution_extraction.py
+++ dpstar/trip_distribution_extraction.py
@@ -9,25 +9,27 @@
 -------------------------------------
 """
 
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 
 from utils import ProgressBar
 
 
-def trip_distribution(trajectory, N, epsilon) -> np.ndarray:
+def trip_distribution(trajs: list, n_grid: int, _epsilon: float) -> np.ndarray:
     """
+
     获取转移概率矩阵
+
     Args:
-        trajectory: 轨迹数据(二维数组)
-        N         : 二级网格数
-        epsilon   : 隐私预算
+        trajs  : 轨迹数据(二维数组)
+        n_grid : 二级网格数
+        _epsilon: 隐私预算
+
     Returns:
         R: 转移概率矩阵
+
     """
-    R = np.zeros((N, N))  # 建立 N*N 的转移概率矩阵
-    for t in trajectory:
+    R = np.zeros((n_grid, n_grid))  # 建立 n_grid * n_grid 的转移概率矩阵
+    for t in trajs:
         if len(t) > 1:
             sta = t[0]
             end = t[-1]
@@ -35,11 +37,11 @@ def trip_distribution(trajectory, N, epsilon) -> np.ndarray:
 
     count = int(np.sum(R))  # 轨迹条数
 
-    p = ProgressBar(N, '生成转移概率矩阵')
-    for i in range(N):
+    p = ProgressBar(n_grid, '生成转移概率矩阵')
+    for i in range(n_grid):
         p.update(i)
-        for j in range(N):
-            noise = np.random.laplace(0, 1 / epsilon)  # 添加拉普拉斯噪声
+        for j in range(n_grid):
+            noise = np.random.laplace(0, 1 / _epsilon)  # 添加拉普拉斯噪声
             R[i][j] += noise
 
             if R[i][j] < 0:
@@ -47,37 +49,36 @@ def trip_distribution(trajectory, N, epsilon) -> np.ndarray:
 
     R /= count
 
-    # 绘制转移概率矩阵热力图
-    sns.heatmap(data=R, square=True)
-    plt.title('trip distribution (epsilon=%s)' % str(used_pair[0]))
-    plt.show()
-
     return R
 
 
-def trip_distribution_main(A, epsilon, src_file, out_file):
+def trip_distribution_main(n_grid: int, _epsilon: float, grid_trajs_path: str,
+                           trip_distribution_path: str):
     """
+
     主函数(将转移概率矩阵写入文件)
+
     Args:
-        A       : 网格数
-        epsilon : 隐私预算
-        src_file: 网格轨迹文件路径
-        out_file: 转移概率矩阵输出文件路径
+        n_grid                : 网格数
+        _epsilon              : 隐私预算
+        grid_trajs_path       : 网格轨迹文件路径
+        trip_distribution_path: 转移概率矩阵输出文件路径
+
     Returns:
+
     """
-    with open(src_file, 'r') as grid_trajectories_file:
+    with open(grid_trajs_path, 'r') as grid_trajs_file:
         # 网格轨迹数据(list)
-        T = [eval(grid_trajectory) for grid_trajectory in grid_trajectories_file.readlines()]
-        with open(out_file, 'w') as trip_distribution_file:
-            trip_distribution_matrix = trip_distribution(T, A, epsilon)
+        T = [eval(grid_traj) for grid_traj in grid_trajs_file.readlines()]
+        with open(trip_distribution_path, 'w') as trip_distribution_file:
+            trip_distribution_matrix = trip_distribution(T, n_grid, _epsilon)
             for item in trip_distribution_matrix:
                 each_line = ' '.join([str(i) for i in item]) + '\n'
                 trip_distribution_file.writelines(each_line)
 
 
 if __name__ == '__main__':
-    ep_grid_pairs = ((0.1, 67), (0.5, 120), (1.0, 193), (2.0, 364))
-    used_pair = ep_grid_pairs[0]
-    trip_distribution_main(used_pair[1], used_pair[0] * 1 / 9,
-                           f'../data/Geolife Trajectories 1.3/middleware/grid_traj_MDL1100_ep{used_pair[0]}.txt',
-                           f'../data/Geolife Trajectories 1.3/middleware/trip_distribution_MDL1100_ep{used_pair[0]}.txt')
+    epsilon = 0.1
+    trip_distribution_main(64, 0.1 * 3 / 9,
+                           f'../data/Geolife Trajectories 1.3/Middleware/grid_trajs_epsilon_{epsilon}.txt',
+                           f'../data/Geolife Trajectories 1.3/Middleware/trip_distribution_epsilon_{epsilon}.txt')
